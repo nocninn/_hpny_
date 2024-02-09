2c320f45734f16100227d0e08a6aadbc99a53f61
nocniin <159463398+nocninn@users.noreply.github.com>
2024-02-09 10:59:20 +0700

    Add files via upload
    createcard

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'nocninn💗💗💗';
+const giftUrl = 'https://www.facebook.com/gialinh.vuhoang.35';
+const eventName = 'Happyy Newww Yearr';
+const titleCard = 'FOR YOU💗';
+const contentCard = 'Chúc cậu năm mới vui vẻ, bình an, hạnh phúc, luôn tươi cười, sớm đạt được ước mơ của mình, ít dỗi hơn, ít khóc nhè hơn........và nhanh nhanh gật đầu nhóooooo💗';
+
+const giftImage = 'anhdoita.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
